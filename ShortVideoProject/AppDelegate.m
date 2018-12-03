584a398b47b4a80a7111de9017e0080ee0b34de4
Skyz <13526832115@163.com>
2018-12-03 16:31:47 +0800

    loginNew

--- ShortVideoProject/AppDelegate.m
+++ ShortVideoProject/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"loading.....");
+    NSLog(@"loginNew");
     return YES;
 }
 
