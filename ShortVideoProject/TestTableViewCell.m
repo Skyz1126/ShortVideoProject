6f4e47e4e8e0983bce5e25a0670e9b89dd139273
Skyz <13526832115@163.com>
2018-12-03 16:11:28 +0800

    增加测试tableViewCell

---
+++ ShortVideoProject/TestTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TestTableViewCell.m
+//  ShortVideoProject
+//
+//  Created by 河南金途科技集团股份有限公司 on 2018/12/3.
+//  Copyright © 2018年 河南金途科技集团股份有限公司. All rights reserved.
+//
+
+#import "TestTableViewCell.h"
+
+@implementation TestTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
